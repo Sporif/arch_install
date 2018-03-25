8b1398f42d339c09d4f7ea5813c82e5813f6ab62
Sporif <Sporif@users.noreply.github.com>
2018-03-25 18:20:51 +0100

    update

--- install.sh
+++ install.sh
@@ -124,8 +124,8 @@ format_partition() {
     BOOT_PART=${DISK}2
     ROOT_PART=${DISK}1
     echo
-    echo BOOT_PART
-    echo ROOT_PART
+    echo $BOOT_PART
+    echo $ROOT_PART
     echo
     mkfs.vfat -F32 $BOOT_PART
     mkfs.ext4 $ROOT_PART
